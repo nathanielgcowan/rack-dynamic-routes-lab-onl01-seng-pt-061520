0bd5430ed57fe4d6e7ce6e5cf957b320ac777177
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-15 14:42:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path=="/items/<ITEM NAME>"
-      resp.write "You requested the songs"
+      resp.write "@@items price"
     else
       resp.write "Route not found"
       resp.status = 404
