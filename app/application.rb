47801d590379677e0885403334d042fbdb6fec35
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-15 14:48:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,11 +8,11 @@ class Application
  
     if req.path=="/items/"
       item_name = req.path.split("/items/").last 
-      if 
-      resp.write "<ITEM PRICE>"
+      if item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
     else
-      resp.write "Route not found"
       resp.status = 404
+      resp.write "Route not found"
     end
  
     resp.finish
