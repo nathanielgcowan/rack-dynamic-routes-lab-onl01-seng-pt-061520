ece939e6eb0ef793af42b75f3a028eb8cf7cdb2c
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-15 14:45:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path=="/items/<ITEM NAME>"
+    if req.path=="/items/"
       resp.write "<ITEM PRICE>"
     else
       resp.write "Route not found"
