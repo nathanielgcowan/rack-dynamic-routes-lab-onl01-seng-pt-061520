64b7472faa830f992d11e2ab4d09a3b1f51647c1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-15 14:46:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path=="/items/"
-      item_name = 
+      item_name = req.path.split
       resp.write "<ITEM PRICE>"
     else
       resp.write "Route not found"
